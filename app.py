ff1618d55ddf2457c627ea4a753866c42291e17d
adi457 <138023777+adi457@users.noreply.github.com>
2023-11-22 20:46:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 # MySQL Configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_PORT'] = 3306
 app.config['MYSQL_DB'] = 'librarydb'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
@@ -753,4 +753,4 @@ def search_book():
 
 if __name__ == '__main__':
     app.secret_key = "secret"
-    app.run(debug=True)
+    app.run(debug=True,port=5500)
